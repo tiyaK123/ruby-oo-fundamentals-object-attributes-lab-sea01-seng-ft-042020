b2f195b1aa5d10c5186bbf56f75e69a2d48f94c4
tiya adugna <tiyakefalee@gmail.com>
2020-04-22 00:27:58 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,12 +1,18 @@
 
 class Dog 
-  def initialize( name)
+  def initialize(breed, name)
     @name = name
-   
+   @breed = beed 
   end
   
   def name 
     @name 
+  end 
+  def breed
+    @breed
+  end
+  def breed=(breeds)
+    @breed = breeds 
   end 
   def name=(names)
     @name = names 
